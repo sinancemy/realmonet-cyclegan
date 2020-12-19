4dc1e77457686e96f3d096507e7ce18b971daa77
Sierox <sinancem_yucel@hotmail.com>
2020-12-20 00:46:38 +0300

    Minor changes to dataset manager

--- src/dataset/manager.py
+++ src/dataset/manager.py
@@ -10,9 +10,13 @@ PAINTING_DST_DIR = "data/testing_set"
 INPUT_W = INPUT_H = 1024
 
 
-def build():
+def generate():
     gen.filter_and_crop(PHOTO_SRC_DIR, PHOTO_DST_DIR, INPUT_W, INPUT_H)
     gen.filter_and_crop(PAINTING_SRC_DIR, PAINTING_DST_DIR, INPUT_W, INPUT_H)
+
+
+def build():
+    pass
 
 
 def load():
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import dataset.manager as dataset_
 
 os.chdir("..")
 
-REBUILD_DATASET = True
+REBUILD_DATASET = False
 
 if REBUILD_DATASET:
     dataset_.build()
