5dfb2b4bae1457331378c508ee4604109f5a1832
Sierox <sinancem_yucel@hotmail.com>
2020-12-20 14:41:09 +0300

    Minor changes

--- src/dataset/manager.py
+++ src/dataset/manager.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from . import generator as gen
 
-PHOTO_SRC_DIR = "data/generator/raw_photos"
-PHOTO_DST_DIR = "data/generator/set_photos"
-PAINTING_SRC_DIR = "data/generator/raw_paintings"
-PAINTING_DST_DIR = "data/generator/set_paintings"
+RAW_PHOTO_DIR = "data/generator/raw_photos"
+SET_PHOTO_DIR = "data/generator/set_photos"
+RAW_PAINTING_DIR = "data/generator/raw_paintings"
+SET_PAINTING_DIR = "data/generator/set_paintings"
 DATA_RES = (1024, 1024)
 
 
 def generate():
     """Runs the generator to convert raw images to dataset images."""
-    gen.process_directory(PHOTO_SRC_DIR, PHOTO_DST_DIR, DATA_RES)
-    gen.process_directory(PAINTING_SRC_DIR, PAINTING_DST_DIR, DATA_RES)
+    gen.process_directory(RAW_PHOTO_DIR, SET_PHOTO_DIR, DATA_RES)
+    gen.process_directory(RAW_PAINTING_DIR, SET_PAINTING_DIR, DATA_RES)
 
 
 def build():
